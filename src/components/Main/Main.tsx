c04147874612ab1041956b68a26d0c611a053986
beznavorotov <beznavorotov@gmail.com>
2024-04-13 20:20:23 +0300

    first commit

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import './main.scss';
+
+function Main() {
+  return (
+    <div className="">
+
+    
+      <h1>Hello world!</h1>
+
+    </div>
+  )
+}
+
+export default Main;
---
+++ src/data.ts
@@ -0,0 +1,71 @@
+export const menu = [
+    {
+        id: 1,
+        title: "MAIN MENU",
+        listItem: [
+            {
+                id: 1,
+                title: "Dashboard",
+                url: "/",
+                icons: "/icons/ic_dashboard.svg",
+                alt: "Dashboard",
+            },
+            {
+                id: 2,
+                title: "Recruitment",
+                url: "/",
+                icons: "/icons/ic_recruitment.svg",
+                alt: "Recruitment",
+            },
+            {
+                id: 3,
+                title: "Schedule",
+                url: "/",
+                icons: "/icons/ic_calendar.svg",
+                alt: "Schedule",
+            },
+            {
+                id: 4,
+                title: "Employee",
+                url: "/",
+                icons: "/icons/ic_employee.svg",
+                alt: "Employee",
+            },
+            {
+                id: 5,
+                title: "Department",
+                url: "/",
+                icons: "/icons/ic_department.svg",
+                alt: "Department",
+            },
+            {
+                id: 6,
+                title: "",
+                url: "",
+                icons: "",
+                alt: "",
+            },
+
+        ]
+    },
+    {
+        id: 2,
+        title: "OTHER",
+        listItem: [
+            {
+                id: 1,
+                title: "Support",
+                url: "/",
+                icons: "/icons/ic_support.svg",
+                alt: "Support",
+            },
+            {
+                id: 2,
+                title: "Settings",
+                url: "/",
+                icons: "/icons/ic_settings.svg",
+                alt: "Settings",
+            },
+        ]
+    }
+]
