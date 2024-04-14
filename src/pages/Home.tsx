0c5886f061dd81cdf724bf6588d23830aa44cc8a
beznavorotov <beznavorotov@gmail.com>
2024-04-14 13:48:01 +0300

    add grid box to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,32 @@
-import './home.scss';
+import "./home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>
-        Home</div>
-  )
+    <div className="home">
+      <div className="panel">
+        <span className="search">
+         <span className="search_text">Search</span>  <img src="/icons/union.svg" alt="" /> </span>
+         <div className="info">
+         <img src="/icons/ic_round-notifications.svg" alt="" />
+        <img src="/icons/bi_chat-left-dots-fill.svg" alt="" />
+        <img src="/icons/photo.svg" alt="" />
+        <span>Admirra John</span>
+        <img src="/icons/vector.svg" alt="" />
+         </div>
 
+      </div>
+      <div className="boxes">
+        <div className="box box1">box1</div>
+        <div className="box box2">box2</div>
+        <div className="box box3">box3</div>
+        <div className="box box4">box4</div>
+        <div className="box box5">box5</div>
+        <div className="box box6">box6</div>
+        <div className="box box7">box7</div>
+        <div className="box box8">box8</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
