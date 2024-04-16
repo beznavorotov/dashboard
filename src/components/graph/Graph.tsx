9acc237942cebdb14659c6d9cffaf00b5b5d1c95
beznavorotov <beznavorotov@gmail.com>
2024-04-16 11:20:48 +0300

    add info to box

--- src/components/graph/Graph.tsx
+++ src/components/graph/Graph.tsx
@@ -1,5 +1,53 @@
 // import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
+import { Area, AreaChart, ResponsiveContainer } from "recharts";
 import "./graph.scss";
+
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 2300,
+    pv: 400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 2600,
+    pv: 398,
+    amt: 210,
+  },
+  {
+    name: 'Page C',
+    uv: 2750,
+    pv: 800,
+    amt: 290,
+  },
+  {
+    name: 'Page D',
+    uv: 2550,
+    pv: 908,
+    amt: 1000,
+  },
+  {
+    name: 'Page E',
+    uv: 2500,
+    pv: 800,
+    amt: 181,
+  },
+  {
+    name: 'Page F',
+    uv: 2790,
+    pv: 800,
+    amt: 500,
+  },
+  {
+    name: 'Page G',
+    uv: 3090,
+    pv: 300,
+    amt: 100,
+  },
+];
+
 
 export const Graph = () => {
   return (
@@ -11,25 +59,32 @@ export const Graph = () => {
         <div className="women">96 Women</div>
       </div>
       <div className="diagram">
-        <div>
-        {/* <ResponsiveContainer width="99%" height="100%">
-            <LineChart data={props.chartData}>
-              <Tooltip
-                contentStyle={{ background: "transparent", border: "none" }}
-                labelStyle={{ display: "none" }}
-                position={{ x: 10, y: 70 }}
-              />
-              <Line
-                type="monotone"
-                dataKey={props.dataKey}
-                stroke={props.color}
-                strokeWidth={2}
-                dot={false}
-              />
-            </LineChart>
-          </ResponsiveContainer> */}
+        <div className="charts">
+        <ResponsiveContainer width="99%" height="100%">
+        <AreaChart
+          width={100}
+          height={30}
+          data={data}
+          margin={{
+            top: 3,
+            right: 0,
+            left: 0,
+            bottom: 2,
+            
+          }}
+          
+        >
+          <Area type="monotone" dataKey="uv" stroke="#FF5151" fill="#ffefe7"  
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+
+
         </div>
+        <div className="text">
         <span className="interest">+2% Past month</span>
+        </div>
+
       </div>
     </div>
   );
