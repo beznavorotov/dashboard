b82d7d1d6c40e8ac4839b0c1a94192372ddc2a9a
beznavorotov <111625937+beznavorotov@users.noreply.github.com>
2024-04-18 09:51:31 +0300

    Update data.ts
    Update data

---
+++ src/data.ts
@@ -0,0 +1,205 @@
+export const menu = [
+  {
+    id: 1,
+    title: "MAIN MENU",
+    listItem: [
+      {
+        id: 1,
+        title: "Dashboard",
+        url: "/",
+        icons: "/icons/ic_dashboard.svg",
+        alt: "Dashboard",
+      },
+      {
+        id: 2,
+        title: "Recruitment",
+        url: "/",
+        icons: "/icons/ic_recruitment.svg",
+        alt: "Recruitment",
+      },
+      {
+        id: 3,
+        title: "Schedule",
+        url: "/",
+        icons: "/icons/ic_calendar.svg",
+        alt: "Schedule",
+      },
+      {
+        id: 4,
+        title: "Employee",
+        url: "/",
+        icons: "/icons/ic_employee.svg",
+        alt: "Employee",
+      },
+      {
+        id: 5,
+        title: "Department",
+        url: "/",
+        icons: "/icons/ic_department.svg",
+        alt: "Department",
+      },
+      {
+        id: 6,
+        title: "",
+        url: "",
+        icons: "",
+        alt: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "OTHER",
+    listItem: [
+      {
+        id: 1,
+        title: "Support",
+        url: "/",
+        icons: "/icons/ic_support.svg",
+        alt: "Support",
+      },
+      {
+        id: 2,
+        title: "Settings",
+        url: "/",
+        icons: "/icons/ic_settings.svg",
+        alt: "Settings",
+      },
+    ],
+  },
+];
+
+export const box = [
+  {
+    name: "Available Position",
+    number: "24",
+    title: "4 Urgently needed",
+  },
+];
+
+export const boxTwo = [
+  {
+    name: "Job Open",
+    number: "10",
+    title: "4 Active hirind",
+  },
+];
+export const boxThree = [
+  {
+    name: "New Employees",
+    number: "24",
+    title: "4 Departament",
+  },
+];
+
+export const activity = [
+  {
+    id: 1,
+    title: "Recently Activity",
+    date: "10.40 AM, Fri 10 Sept 2021",
+    content: "You Posted a New Job",
+    text: "Kindly check the requirements and terms of work and make sure everything is right.",
+    textEnd: "Today you makes 12 Activity",
+  },
+];
+
+export const graphOne = [
+  {
+    id: 1,
+    title: "Total Employees",
+    number: "216",
+    men: "120",
+    women: "96",
+    interest: "+2%",
+  },
+];
+
+export const graphTwo = [
+  {
+    id: 1,
+    title: "Talent Request",
+    number: "16",
+    men: "6",
+    women: "10",
+    interest: "+5%",
+  },
+];
+
+export const announcement = [
+  {
+    id: 1,
+    listItem: [
+      {
+        id: 1,
+        title: "Outing schedule for every departement",
+        text: "5 Minutes ago",
+      },
+    ],
+  },
+  {
+    id: 2,
+    listItem: [
+      {
+        id: 1,
+        title: "Meeting HR Department",
+        text: "Yesterday, 12:30 PM",
+      },
+    ],
+  },
+  {
+    id: 3,
+    listItem: [
+      {
+        id: 1,
+        title:
+          "IT Department need two more talents for UX/UI Designer position",
+        text: "Yesterday, 09:15 AM",
+      },
+    ],
+  },
+];
+
+export const upcoming = [
+  {
+    id: 1,
+    title: "Priority",
+    listItem: [
+      {
+        id: 1,
+        title: "Review candidate applications",
+        text: "Today - 11.30 AM",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Other",
+    listItem: [
+      {
+        id: 1,
+        title: "Interview with candidates",
+        text: "Today - 10.30 AM",
+      },
+    ],
+  },
+  {
+    id: 3,
+    listItem: [
+      {
+        id: 1,
+        title: "Short meeting with product designer from IT Departement",
+        text: "Today - 09.15 AM",
+      },
+    ],
+  },
+  {
+    id: 4,
+    listItem: [
+      {
+        id: 1,
+        title: "Sort Front-end developer candidates",
+        text: "Yesterday - 09.15 AM",
+      },
+    ],
+  },
+];
